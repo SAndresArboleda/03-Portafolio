9374588f4f12e29170832c5c7d2f623aa7310693
SAndresArboleda <simondres@yahoo.es>
2024-06-29 10:20:14 -0500

    En HTML

--- src/redux/reducer.js
+++
@@ -1,9 +0,0 @@
-let initialsState = {
-    allProducts: [],
-}
-export const reducer = (state = initialsState, action) => {
-    switch (action.type) {
-        default:
-            return state;
-    }
-}
